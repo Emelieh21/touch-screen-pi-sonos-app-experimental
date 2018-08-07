0e592ab59e216fa8a5b6286b3ff2d7d00ca63bbe
Emelie Hofland <emelie_hofland@hotmail.com>
2018-08-08 00:01:34 +0200

    tiny error

--- camera_python.py
+++ camera_python.py
@@ -4,7 +4,7 @@ from picamera import PiCamera
 
 camera = PiCamera()
 timestamp = str(datetime.now()).replace(" ","_").replace(":","_").replace(".","_")
-filename = 'images/'+timestamp+"_"+sys.argv[1]+'_image.jpg'
+filename = '/home/pi/touch-screen-pi-sonos-app-experimental/images/'+timestamp+"_"+sys.argv[1]+'_image.jpg'
 
 print(filename)
 camera.capture(filename)
